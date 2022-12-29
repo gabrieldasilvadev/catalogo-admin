7a88c34652d29830a28bbabd486745c67dace101
Gabriel <gabriel@pop-os.localdomain>
2022-12-29 19:18:53 -0300

    :construction: Crated entitie category with unit test

--- src/main/kotlin/br/com/gabriel/admin/catalogo/domain/Category.kt
+++ src/main/kotlin/br/com/gabriel/admin/catalogo/domain/Category.kt
@@ -1,4 +1,10 @@
 package br.com.gabriel.admin.catalogo.domain
 
 class Category {
+    var id: String = ""
+
+        get() = field
+        set(value) {
+            field = value
+        }
 }
--- src/test/kotlin/br/com/gabriel/admin/catalogo/domain/CategoryTest.kt
+++ src/test/kotlin/br/com/gabriel/admin/catalogo/domain/CategoryTest.kt
@@ -1,4 +1,19 @@
 package br.com.gabriel.admin.catalogo.domain
 
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
 class CategoryTest {
+    @Test
+    fun `Should create a new instance of Category`() {
+        Assertions.assertNotNull(Category())
+    }
+
+    @Test
+    fun `Should return a new object of Category with id equals 1`() {
+        val category = Category()
+        category.id = "1"
+
+        Assertions.assertEquals("1", category.id)
+    }
 }
